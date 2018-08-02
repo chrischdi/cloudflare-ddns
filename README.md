153e9c5707288996dc9a01e7daf01aec850f7a75
Christian <christian@cschlotter.de>
2018-08-02 23:00:39 +0200

    initial commit of cloudflare-ddns

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # cloudflare-ddns
-Dynamic dns client for cloudflare
+[![Build Status](https://travis-ci.org/chrischdi/cloudflare-ddns.svg?branch=master)](https://travis-ci.org/chrischdi/cloudflare-ddns)
+
+`cloudflare-ddns` is a client to dynamically update dns records on cloudflare.
+
+On its first start it will fetch the current set IP address of the passed dns record and will compare it to the current real public ip address. 
+
+```
+$ cloudflare-ddns -h
+Usage of ./cloudflare-ddns:
+  -public-ip-url string
+    	URL to fetch the current public ip address (default "https://checkip.amazonaws.com/")
+  -record-name string
+    	name of the dns record to update
+  -refresh-interval int
+    	Interval in seconds between record updates (default 300s) (default 300)
+  -zone-name string
+    	name of the dns zone
+```
+
+# Install from source
+
+Install or update from current master:
+
+```
+go get -u github.com/chrischdi/cloudflare-ddns
+```
+
+# Usage
+
+To use `cloudflare-ddns` you have to set the following Cloudflare credentials by environment variables:
+* `CF_API_EMAIL`: your login e-mail address
+* `CF_API_KEY`: your Cloudflare API key
+Additionaly the following cli parameters are mandatory:
+* `-record-name`
+* `-zone-name`
+
+# Contribute
+
+Feel free to clone or fork this repo to start contributing.
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+var (
+	zoneName        = flag.String("zone-name", "", "name of the dns zone")
+	dnsName         = flag.String("record-name", "", "name of the dns record to update")
+	refreshInterval = flag.Int64("refresh-interval", 300, "Interval in seconds between record updates (default 300s)")
+	publicIPURL     = flag.String("public-ip-url", "https://checkip.amazonaws.com/", "URL to fetch the current public ip address")
+)
+
+func main() {
+	flag.Parse()
+
+	if *dnsName == "" {
+		log.Fatalf("error: record-name parameter is mandatory")
+	}
+
+	// Construct a new API object
+	api, err := cloudflare.New(os.Getenv("CF_API_KEY"), os.Getenv("CF_API_EMAIL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	zoneID, err := api.ZoneIDByName(*zoneName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	records, err := api.DNSRecords(zoneID, cloudflare.DNSRecord{Type: "A", Name: *dnsName})
+
+	if len(records) != 1 {
+		log.Fatalf("error: dns record not found")
+	}
+
+	record := records[0]
+
+	var public string
+
+	for {
+		public, err = getPublicIP()
+		public = strings.TrimSuffix(public, "\n")
+		if public != record.Content {
+			record.Content = public
+			err = api.UpdateDNSRecord(zoneID, record.ID, cloudflare.DNSRecord{
+				Content: public,
+			})
+			if err != nil {
+				log.Printf("error updating dns record to %s: %v", public, err)
+			} else {
+				log.Printf("successfully updated ip from to %s", public)
+			}
+		} else {
+			log.Printf("no update needed")
+		}
+		time.Sleep(time.Second * time.Duration(*refreshInterval))
+	}
+}
+
+// getPublicIP returns the internet facing public ip address
+func getPublicIP() (string, error) {
+	resp, err := http.Get(*publicIPURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	ip, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(ip), nil
+}
